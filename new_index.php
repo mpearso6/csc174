8e7119c6de11496637aa8c68c6812acb25ecf725
Dominique Dorvil <decile14@gmail.com>
2019-09-11 16:10:29 -0400

    created new index/ home page

---
+++ new_index.php
@@ -0,0 +1,19 @@
+<?php $pageTitle = "Team Havana | Lab 2: The Mash Up"; ?>
+<?php include "inc/html-top.inc"; ?>
+
+<body class="container">
+
+<?php include "inc/nav.inc"; ?>
+
+<main>
+	
+	<p> 
+		Hi! We are Team Havana It’s a pleasure to meet you! 🤗 </p>
+ 
+</main>
+
+<?php include "inc/footer.inc"; ?>
+<?php include "inc/scripts.inc"; ?>
+
+</body>
+</html>
