2ac9cc422708662b31bc709e8de3d0914d1af03f
vacevedo44 <55210725+vacevedo44@users.noreply.github.com>
2019-09-11 16:04:57 -0400

    Add files via upload

---
+++ assignment01/inc/html-top.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Vanessa A. Acevedo | Lab 1: Initial Personal Website</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="container">
+	<header>
+		<a href="index.php">
+			<h1>Vanessa Acevedo</h1>
+			<img src="images/me.jpg" alt="My face">
+		</a>
+	</header>
+
+	<aside>
+		<ul>
+			<li>English Major at a STEM School</li>
+			<li>Dog Mom</li>
+			<li>Lover of Web Design</li>
+		</ul>
+	</aside>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="fall.php">Fall 2018</a></li>
+			<li><a href="spring.php">Spring 2019</a></li>
+			<li><a href="today.php">Now</a></li>
+		</ul>
+	</nav>
+
---
+++ assignment01/index.php
@@ -0,0 +1,15 @@
+<?php include "inc/html-top.php"; ?> 
+
+	<main>
+		<h1>Me, in a Nutshell</h1>
+
+		<p>I am currently a sophomore at the University of Rochester pursing a Bachelor’s degree in Language, Media, and Communications (or English, in simpler terms). I’ve essentially spent my entire life in Rochester as I was born and grew up here. After obtaining my Bachelor’s degree, I plan on attending the Warner School of Education here at UR to get a Master’s in Education. My career goal is to become a secondary English teacher, and I would really love to teach at an RCSD school, which is where I attended both elementary and high school. Aside from English, I’m also very interested in Computer Science, Studio Art and Gender Studies, which I have been able to explore more through classes here at UR. </p>
+		
+	</main>
+
+	<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
+
+	<?php include "inc/scripts.php"; ?>
+
+</body>
+</html>
