06de41cb4443c7dd155bbe6a0d7cb43a25d86eaf
Dominique Dorvil <decile14@gmail.com>
2019-09-11 16:38:16 -0400

    new offical page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php $pageTitle = "Team Havana | Lab 2: The Mash Up"; ?>
+<?php include "inc/html-top.inc"; ?>
+
+<body class="container">
+
+<?php include "inc/nav.inc"; ?>
+
+<main>
+	
+	<p> 
+		Hi! We are Team Havana It’s a pleasure to meet you! 🤗 </p>
+ 
+</main>
+
+<ul>
+	<li><a href="dom_index.php">Home</a></li>
+	<li><a href="v_index.php">Home</a></li>
+	<li><a href="mp_index.php">Home</a></li>
+	
+
+
+</ul>
+
+
+<?php include "inc/footer.inc"; ?>
+<?php include "inc/scripts.inc"; ?>
+
+</body>
+</html>
