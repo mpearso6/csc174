910a9715cf7f0900a4c4117b30a35a544d570aa8
mpearso6 <44856312+mpearso6@users.noreply.github.com>
2019-09-11 16:03:15 -0400

    Add files via upload

---
+++ mpearso6/lab01/future.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Future Plans |Michael Pearson | Lab 1: Initial Personal Website</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+	<?php include("header.php"); ?>
+<body class = "container">
+	<main>
+		<h1>Future Plans</h1>
+		<p>
+		My Name is Michael Pearson and I was born and raised in Washington D.C. Ever since I could remember I was always fascinated with technology. It all started playing with legos and growing up playing a bunch of games. Playing the games itself added to my address of technology but the world building inside the games I played were astonishing. For example, Star Wars, Halo, Gears of War, and so many more. I am also big fan of sci fi films which contributed to my passion for technology. 
+			</p>
+	</main>
+
+
+	<?php $current = 'future'; include("menu.php"); ?>
+
+</body>
+</html>
---
+++ mpearso6/lab01/menu.php
@@ -0,0 +1,13 @@
+
+	<nav>
+
+			<ul>
+				<li><a <?php if($current == 'home') {echo 'class="active"';} ?> href="index.php">Home</a></li>
+				<li><a <?php if($current == 'Interest') {echo 'class="active"';} ?> href="interest.php">Interest</a></li>
+				<li><a <?php if($current == 'college') {echo 'class="active"';} ?> href="college.php">College Life</a><li>
+				<li><a <?php if($current == 'future') {echo 'class="active"';} ?> href="future.php">Future</a></li>
+			</ul>
+
+	</nav>
+
+		<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
